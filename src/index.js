e9a9ece7a06bb75bad66f9f43f7acdb6afb61709
Dennis W. Otugo <otugodennis@gmail.com>
2017-11-18 13:14:37 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AudioPlayer from './AudioPlayer';
+import registerServiceWorker from './registerServiceWorker';
+
+const songs = [
+  {
+    url: './fire.mp3',
+    artist: {
+      name: 'Fireplace'
+    }
+  },
+  {
+    url: './rain.mp3',
+    artist: {
+      name: 'Rainy'
+    }
+  },
+  {
+    url: './wind.mp3',
+    artist: {
+      name: 'Wind'
+    }
+  },
+  {
+    url: './coffee.mp3',
+    artist: {
+      name: 'Coffee Shop'
+    }
+  },
+  {
+    url: './night.mp3',
+    artist: {
+      name: 'Night'
+    }
+  },
+  {
+    url: './static.mp3',
+    artist: {
+      name: 'Static'
+    }
+  }
+];
+
+ReactDOM.render(<AudioPlayer songs={songs} />, document.getElementById('root'));
+registerServiceWorker();
