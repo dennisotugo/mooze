f623194e1fe15aef864d8057d1a8b22a3ec2ab9a
Wesley Otugo <wesleyotugo@gmail.com>
2017-11-19 20:04:53 +0100

    .

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  // Entry files for our popup and background pages
+  entry: {
+    index: './src/index.js',
+    registerServiceWorker: './src/registerServiceWorker.js',
+    AudioPlayer: './src/AudioPlayer.js'
+  },
+  // Extension will be built into ./dist folder, which we can then load as unpacked extension in Chrome
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  // Here we define loaders for different file types
+  module: {
+    rules: [
+      // We use Babel to transpile JSX
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, './src')
+        ],
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.(ico|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
+        use: 'file-loader?limit=100000'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?limit=100000',
+          {
+            loader: 'img-loader',
+            options: {
+              enabled: true,
+              optipng: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // create CSS file with all used styles
+    new ExtractTextPlugin('bundle.css'),
+    // create popup.html from template and inject styles and script bundles
+    new HtmlWebpackPlugin({
+      inject: true,
+      chunks: ['index.html'],
+      filename: 'index.html',
+      template: './public/index.html'
+    }),
+    // copy extension manifest and icons
+    new CopyWebpackPlugin([
+      { from: './public/manifest.json' },
+      { context: './src/assets', from: 'icon-**', to: 'assets' }
+    ])
+  ]
+}
